28633b6bae8427a3cfe296ba376ec61310737a83
Lucas McDaniel <ldmcdaniel@gmail.com>
2015-06-24 10:26:15 -0500

    working mouse selector

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,4 @@
-var map, water, sand, grass, trees, bridges, game, PhaserGame;
+var map, water, sand, grass, trees, bridges, game, marker, layer, cursors, PhaserGame, currentTile;
 
 game = new Phaser.Game(1024, 768, Phaser.AUTO, 'game');
 
@@ -19,8 +19,10 @@ PhaserGame.prototype = {
 
   create: function () {
     this.map = this.add.tilemap('map');
-      //The first param is the name of the tileset from tiled; the second is from game.load.image
+
+    //The first param is the name of the tileset from tiled; the second is from game.load.image
     this.map.addTilesetImage('terrain_atlas', 'tiles');
+    this.currentTile = this.map.getTile(1, 1);
 
     //These are the layers from my map
     this.water = this.map.createLayer('River');
@@ -28,6 +30,7 @@ PhaserGame.prototype = {
     this.grass = this.map.createLayer('Grass');
     this.trees = this.map.createLayer('Tree bases');
 
+    //Enemy sprite and travel path information
     this.test = this.add.sprite(-16, 116, 'boat');
     this.test.scale.setTo(2, 2);
     this.test.anchor.set(0.5);
@@ -36,18 +39,33 @@ PhaserGame.prototype = {
     this.plot();
     this.bridges = this.map.createLayer('Tree Tops and Bridges');
 
+    //Mouse marker
+    marker = game.add.graphics();
+    marker.lineStyle(2, 0x000000, 1);
+    marker.drawRect(0, 0, 32, 32);
+
+
+    this.cursors = game.input.keyboard.createCursorKeys();
+
   },
 
   plot: function () {
+
+    //Path plot info for the enemy sprites
     this.path = [];
     var ix = 0;
-    var x = 1 / 500;
+    var x = 1 / game.width;
 
     for (var i = 0; i <= 1; i += x) {
       var px = this.math.catmullRomInterpolation(this.points.x, i);
       var py = this.math.catmullRomInterpolation(this.points.y, i);
+
+      //This code was before I put the angle rotation of the ships into the code
       // this.path.push( { x: px, y: py });
+
+      //This draws the path onto the screen to edit the path
       // this.bmd.rect(px, py, 1, 1, 'rgba(255, 255, 255, 1)');
+
       var node = {x: px, y: py, angle: 0};
       if (ix > 0) {
         node.angle = this.math.angleBetweenPoints(this.path[ix - 1], node);
@@ -56,12 +74,19 @@ PhaserGame.prototype = {
       ix++;
     }
 
+    //This code draws rectangles onto the path where the nodes are located
     // for (var p = 0; p < this.points.x.length; p++) {
     //   this.bmd.rect(this.points.x[p]-3, this.points.y[p]-3, 6, 6, 'rgba(255, 0, 0, 1)');
     // }
 
   },
   update: function (){
+
+    //
+    marker.x = this.grass.getTileX(game.input.activePointer.worldX) * 32;
+    marker.y = this.grass.getTileY(game.input.activePointer.worldY) * 32;
+
+    //Information for the path of enemy sprite
     this.test.x = this.path[this.pi].x;
     this.test.y = this.path[this.pi].y;
     this.test.rotation = this.path[this.pi].angle;
